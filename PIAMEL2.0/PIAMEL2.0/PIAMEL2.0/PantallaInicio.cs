14c2520ce53cb7ccf812c4affaa6e68cc69a2dfe
Henchin117 <112787591+Henchin117@users.noreply.github.com>
2022-11-12 20:40:26 -0600

    Add files via upload

---
+++ PIAMEL2.0/PIAMEL2.0/PIAMEL2.0/PantallaInicio.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace PIAMEL2._0
+{
+    public partial class PantallaInicio : Form
+    {
+        public PantallaInicio()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection coneccion = new SqlConnection("server= DESKTOP-C08OI78\\SQLEXPRESS ; database = SITEMA ; INTEGRATED SECURITY = true");
+
+        private void btn_login_Click(object sender, EventArgs e)
+        {
+            coneccion.Open();
+            SqlCommand comando = new SqlCommand("SELECT [USER], PASWORD FROM LOGIN WHERE [USER] = @vUser AND PASWORD = @vPassword", coneccion);
+            comando.Parameters.AddWithValue("@vUser", txt_user.Text);
+            comando.Parameters.AddWithValue("@vPassword", txt_pass.Text);
+
+            SqlDataReader lector = comando.ExecuteReader();
+            if (lector.Read())
+            {
+                coneccion.Close();
+                Form11 pantalla = new Form11();
+                pantalla.Show();
+            }
+
+        }
+    }
+}
---
+++ PIAMEL2.0/PIAMEL2.0/PIAMEL2.0/PantallaInicio.designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace PIAMEL2._0
+
+{
+    partial class PantallaInicio
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_user = new System.Windows.Forms.TextBox();
+            this.txt_pass = new System.Windows.Forms.TextBox();
+            this.lbl_user = new System.Windows.Forms.Label();
+            this.lbl_pass = new System.Windows.Forms.Label();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt_user
+            // 
+            this.txt_user.Location = new System.Drawing.Point(76, 111);
+            this.txt_user.Name = "txt_user";
+            this.txt_user.Size = new System.Drawing.Size(190, 20);
+            this.txt_user.TabIndex = 0;
+            // 
+            // txt_pass
+            // 
+            this.txt_pass.Location = new System.Drawing.Point(76, 201);
+            this.txt_pass.Name = "txt_pass";
+            this.txt_pass.PasswordChar = '*';
+            this.txt_pass.Size = new System.Drawing.Size(190, 20);
+            this.txt_pass.TabIndex = 3;
+            // 
+            // lbl_user
+            // 
+            this.lbl_user.AutoSize = true;
+            this.lbl_user.Font = new System.Drawing.Font("Segoe UI", 20F);
+            this.lbl_user.Location = new System.Drawing.Point(123, 63);
+            this.lbl_user.Name = "lbl_user";
+            this.lbl_user.Size = new System.Drawing.Size(108, 37);
+            this.lbl_user.TabIndex = 4;
+            this.lbl_user.Text = "Usuario";
+            // 
+            // lbl_pass
+            // 
+            this.lbl_pass.AutoSize = true;
+            this.lbl_pass.Font = new System.Drawing.Font("Segoe UI", 20F);
+            this.lbl_pass.Location = new System.Drawing.Point(103, 159);
+            this.lbl_pass.Name = "lbl_pass";
+            this.lbl_pass.Size = new System.Drawing.Size(151, 37);
+            this.lbl_pass.TabIndex = 5;
+            this.lbl_pass.Text = "Contraseña";
+            // 
+            // btn_login
+            // 
+            this.btn_login.Location = new System.Drawing.Point(96, 244);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(158, 23);
+            this.btn_login.TabIndex = 6;
+            this.btn_login.Text = "Iniciar Sesion";
+            this.btn_login.UseVisualStyleBackColor = true;
+            this.btn_login.Click += new System.EventHandler(this.btn_login_Click);
+            // 
+            // PantallaInicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(346, 279);
+            this.Controls.Add(this.btn_login);
+            this.Controls.Add(this.lbl_pass);
+            this.Controls.Add(this.lbl_user);
+            this.Controls.Add(this.txt_pass);
+            this.Controls.Add(this.txt_user);
+            this.Name = "PantallaInicio";
+            this.Text = "IniciodeSesion";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_user;
+        private System.Windows.Forms.TextBox txt_pass;
+        private System.Windows.Forms.Label lbl_user;
+        private System.Windows.Forms.Label lbl_pass;
+        private System.Windows.Forms.Button btn_login;
+    }
+}
